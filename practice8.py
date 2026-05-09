7ff5271a15dfc2c7bedfad7d162fc00be237c75e
Md tuhin <tuhinraja433@gmail.com>
2026-05-09 18:01:51 +0600

    add new codes

--- practice8.py
+++ practice8.py
@@ -6,11 +6,11 @@ def main():
     UserInputs("users_database.txt")
 
 def UserInputs(file):
-    print("Wellcome to noteHuk...\n press number to add/remove/update/see\n Add -- [press 1],\n Watch -- [press 2],\n Update -- [press 3] and\n Delete -- [press 4]\n Show last note -- [press 5]\n Show first one -- [press 6] and show new all notes to (press 7)\n")
+    print("Wellcome to noteHuk...\n press number to add/remove/update/see\n Add -- [press 1],\n Watch -- [press 2],\n Update -- [press 3] and\n Delete -- [press 4]\n Show last note -- [press 5]\n Show first one -- [press 6],\n show new all notes -- (press 7)\n read delete history -- (press 8)\n")
     input_userExp = 0
     while True:
         input_userExp = int(input("Press 1/2/3/4/... ? "))
-        if input_userExp <= 7 and input_userExp > 0:
+        if input_userExp <= 8 and input_userExp > 0:
             break
         # IF NOT ERR
     __HANDLE_INPUT_PROMPT(input_userExp, file) 
@@ -56,8 +56,20 @@ def __HANDLE_INPUT_PROMPT(input_n, working_file):
              __READ(working_file,0, "first")
         elif input_n == 7:
              __READ(working_file,0, "new")
+        elif input_n == 8:
+             delete_history()
         else:
              print("Wrong input type !")
+def delete_history():
+     history = __GET_HISTORY()
+     if len(history) < 1:
+          print("Empty delete history!")
+          return
+     print("---- Your deleted notes ----\n")
+     __READ("delete_history.txt", 0, "all")
+def open_spacific_note(id):
+     __READ("users_database.txt", id, "single")
+
 def input_notes_part(__mode):
      if __mode == 0:
           return [str(input("Enter update title: "))]
@@ -142,7 +154,7 @@ def open_note(file):
           if int(read_notes[i][0]) == open_id:
                is_found = True
      if is_found:
-          print()
+          open_spacific_note(open_id)
      else:
           print("Not found this note !")
 
@@ -155,6 +167,7 @@ def __DELETE_FILE_CONTENT(filename, deleteId):
              __count_readed = int(read.split(" |-13-@3*3code(013.1-qpd__)| ")[0])
              if __count_readed == deleteId:
                   __IS_DELETED = True
+                  __SET_HISTORY(read)
              else:
                   __DATA_STACK__.append(read)        
         # delete note from file           
@@ -206,7 +219,7 @@ def __ADD(file):
         __WRITE_FILE(file, f"{len(file_data)+1} |-13-@3*3code(013.1-qpd__)| {note_title if note_title else '---'} |-13-@3*3code(013.1-qpd__)| {note__ if note__ else ''} |-13-@3*3code(013.1-qpd__)| {time_config.time()} , {time_config.day()} , {time_config.month()} , {time_config.year()} |-13-@3*3code(013.1-qpd__)| 0 , {time_config.time()} , 0 , 0 \n")
      
 def __READ(file,id,filter="all"):
-    filter_list = ["all", "new", "old", "last", "first"]
+    filter_list = ["all", "new", "old", "last", "first", "single"]
     __time = __show_time()
     if filter in filter_list and filter == "all" and id == 0:
         __read_file = __READ_FILE(file)
@@ -222,20 +235,19 @@ def __READ(file,id,filter="all"):
         __count = 0
         for line in __read_file:
              edited_checker_list = line.split(" |-13-@3*3code(013.1-qpd__)| ")[4].split(" , ")
-          #    {split_line[3].split(' , ')[3]}, {split_line[3].split(' , ')[1][0:3]},
              split_line = line.split(" |-13-@3*3code(013.1-qpd__)| ")
              print(f"created at -- {__time.calc_time(float((line.split(' |-13-@3*3code(013.1-qpd__)| '))[3].split(' , ')[0]))[1]} ago {f'(edited: {__time.calc_time(float(edited_checker_list[1]))[1]})' if int(edited_checker_list[0]) == 1 else ''}")
              print_line = ""
              for i in range(0, len(split_line)-2):
                   print_line += split_line[i]
                   if i < 2:
-                       print_line += ' | '
+                    print_line += ' | '
+                  
              print(print_line)
              if __count+1 != len(__read_file):
                   print("\n ------- \n")
              __count += 1
              
-                  
     elif filter in filter_list and filter == "last":
          __read_file = __READ_FILE(file)
          print(f"Created at -- {__time.calc_time(float((__read_file[len(__read_file)-1].split(' |-13-@3*3code(013.1-qpd__)| '))[3].split(' , ')[0]))}")
@@ -255,7 +267,32 @@ def __READ(file,id,filter="all"):
                     has_list = True
          if not has_list:
               print("Not found new notes !")
-                   
+    elif filter in filter_list and filter == "single" and id > 0:
+          read_list = __READ_FILE_CONTENT_TO_LIST(file)
+          read_list_index = -1
+          is_found = False
+          for i in range(0, len(read_list)):
+               if int(read_list[i][0]) == id:
+                    is_found = True
+                    read_list_index = i
+          if is_found:
+               note = read_list[read_list_index]
+               date__ = note[3].split(" , ")
+               edit_details = note[4].split(" , ")
+               print(f"\n[{id}]--created--[ {date__[1][0:3]}, {__time.calc_time(float(date__[0]))[1]} ago ] {f'-- (edited at {__time.calc_time(float(edit_details[1]))[0]})' if int(edit_details[0]) == 1 else ''}")
+               print(" -------------- ")
+               print(f"• {note[1]}")
+               print(" -------------- ")
+               print(f"• {note[2]}\n")
+               print(f"press 0 to see details? ")
+               code__ = int(input("Enter code: "))
+               if code__ == 0:
+                    print(f"id: {note[0]}")
+                    print(f"created at {date__[1][0:3]}, {__time.calc_time(float(date__[0]))[1]} ago ]")
+                    print(f"is edited: {int(edit_details[0]) == 1} and edit {'title' if int(edit_details[2]) == 1 and int(edit_details[0]) == 1 else ''}{', note' if int(edit_details[3]) == 1 and int(edit_details[0]) == 1 else ''} ")
+                    print(f"Year: {date__[3]}, Month: {date__[2]}, day: {date__[1][0:3]}")
+                    print(f"Activity: {__time.calc_time(float(date__[0]))}")
+
 def __READ_FILE_CONTENT_TO_LIST(filename):
      __USER_FILE_DATA = []
      with open(filename, "r") as read_data_file:
@@ -264,6 +301,14 @@ def __READ_FILE_CONTENT_TO_LIST(filename):
      return __USER_FILE_DATA
 
      
-
+def __SET_HISTORY(write,file="delete_history.txt"):
+     with open(file, "a+") as write_his: 
+          write_his.write(write)
+def __GET_HISTORY(file="delete_history.txt"):
+     __data_list = []
+     with open(file, "r") as read_his:
+          for read in read_his:
+               __data_list.append(read)
+     return __data_list
 if __name__ == "__main__":
     main()
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+open_spacific_note
